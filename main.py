a398b4c2e288671a7e655132641598af0405a2c4
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2023-05-05 18:22:30 +0900

    中村さんその音声再生機能追加

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import requests, json, io, wave, pyaudio, time
+import requests
+import json
+import io
+import wave
+import pyaudio
+import time
+from requests.exceptions import Timeout
 
 def main():
-    host = "127.0.0.1"
-    port = 50021
+    requests_info = {
+        "host": "localhost",
+        "port": 50021
+    }
+    text = "中村さんそは眠らない"
+    
+    sample_file = "sanso.wav"
+    sounds = Sounds(sample_file)
+    # sounds.playwavfile()
 
-    inst = Voicevox(host, port)
-    inst.speak(text="ここにメッセージを入力")
+    voicevox = Voicevox(requests_info["host"], requests_info["port"])
+    voicevox.speak(text=text)
 
 class Voicevox:
     def __init__(self,host="127.0.0.1",port=50021):
@@ -15,26 +28,31 @@ class Voicevox:
     def speak(self,text=None,speaker=54): # spaker_id = ななひら
         params = (
             ("text", text),
-            ("speaker", speaker)  # 音声の種類をInt型で指定
+            ("speaker", speaker)
         )
 
-        init_q = requests.post(
-            f"http://{self.host}:{self.port}/audio_query",
-            params=params
-        )
+        try:
+            init_q = requests.post(
+                f"http://{self.host}:{self.port}/audio_query",
+                params=params
+            )
 
-        res = requests.post(
-            f"http://{self.host}:{self.port}/synthesis",
-            headers={"Content-Type": "application/json"},
-            params=params,
-            data=json.dumps(init_q.json())
-        )
+            res = requests.post(
+                f"http://{self.host}:{self.port}/synthesis",
+                headers={"Content-Type": "application/json"},
+                params=params,
+                data=json.dumps(init_q.json())
+            )
+        # http request error
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return False
 
-        # メモリ上で展開
+        # メモリ展開
         audio = io.BytesIO(res.content)
 
         with wave.open(audio,'rb') as f:
-            # 以下再生用処理
+            # 音声再生処理
             p = pyaudio.PyAudio()
 
             def _callback(in_data, frame_count, time_info, status):
@@ -47,7 +65,7 @@ class Voicevox:
                             output=True,
                             stream_callback=_callback)
 
-            # Voice再生
+            # 音声再生
             stream.start_stream()
             while stream.is_active():
                 time.sleep(0.1)
@@ -57,6 +75,39 @@ class Voicevox:
             p.terminate()
 
 
+class Sounds:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def playwavfile(self):
+        try:
+            wf = wave.open(self.filename, "r")
+        except Exception as e:
+            print(e)
+            return False
+            
+        # ストリームを開く
+        try:
+            p = pyaudio.PyAudio()
+            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                            channels=wf.getnchannels(),
+                            rate=wf.getframerate(),
+                            output=True)
+
+            # 音声再生
+            chunk = 1024
+            data = wf.readframes(chunk)
+            while data != '':
+                stream.write(data)
+                data = wf.readframes(chunk)
+            stream.close()
+            p.terminate()
+
+            # プロンプトが帰ってきてない(error)
+            return 0
+
+        except Exception as e:
+            print(e)
+
 if __name__ == "__main__":
     main()
-
