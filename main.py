13b033ffa098e71de01a27adb0a1fa826be5157f
nnnaanaa <ryuu_yoshi_22_0417@yahoo.co.jp>
2024-02-14 21:41:49 +0900

    ういびーむ

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from voicevox_client_local import VoicevoxEngineLocal
 
 def main():
     voicevox = VoicevoxEngineLocal()
-    voicevox.http_request(text="ういびーむ")
+    voicevox.http_request(text="インベタのさらにインは空中に描くラインだ。高低差の大きいいろは坂特有のヘアピンカーブだからこそ実現可能な掟破りの地元走りだ。この勝負勝った")
 
 if __name__ == "__main__":
     main()
